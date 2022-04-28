2a7397b22180fd599435acbcd2971d76275bde83
Nomcebo-1 <ndimandenomcebo1@gmail.com>
2022-04-28 11:03:54 +0200

    Add project files.

---
+++ APIIBookApp/Controllers/BooksController.cs
@@ -0,0 +1,121 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIIBookApp.Data;
+using APIIBookApp.Model;
+
+namespace APIIBookApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly APIIBookAppContext _context;
+
+        public BooksController(APIIBookAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBook()
+        {
+            string path = @"C:\Users\bbdnet2627\source\repos\BookAPIApp\File folder\API_File.txt";
+            using (StreamWriter stream = new StreamWriter(path))
+            {
+                int qry = (from Book
+                           in _context.Book select Book).Count();
+
+                stream.WriteLine("Number of Records: " + qry);
+            }
+            return await _context.Book.ToListAsync();
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Book.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // PUT: api/Books/5
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Books
+   
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.Book.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Book.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Book.Any(e => e.Id == id);
+        }
+
+       
+
+
+    }    
+}
---
+++ APIIBookApp/Data/APIIBookAppContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIIBookApp.Model;
+
+namespace APIIBookApp.Data
+{
+    public class APIIBookAppContext : DbContext
+    {
+        public APIIBookAppContext (DbContextOptions<APIIBookAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APIIBookApp.Model.Book> Book { get; set; }
+    }
+}
